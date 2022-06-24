1f83136e3f6fd75051fd33707ba0fe3c8e21ff42
Andrew Christie <awchristie86@gmail.com>
2022-06-23 22:16:28 -0400

    some db stuff

--- ewmd.py
+++ ewmd.py
@@ -1,5 +1,5 @@
 import websockets
-import get_token
+import requests
 import asyncio
 import logging
 
@@ -45,16 +45,38 @@ class WSClient:
         }}
     """
 
+    @staticmethod
+    def get_token(username, password, apikey):
+        """Get an Access Token to make API calls with."""
+        body = {"username": username, "password": password}
+        headers = {"appkey": apikey}
+        try:
+            r = requests.post(
+                "https://api.platts.com/auth/api", data=body, headers=headers
+            )
+            r.raise_for_status()
+            return r.json()["access_token"]
+        except Exception as err:
+            if r.status_code >= 500:
+                logging.error(f"[{r.status_code}] - {err}")
+            else:
+                logging.error(f"[{r.status_code}] -  {r.json()}")
+            raise
+
     async def connect(self):
-        token = get_token.get_token(self.un, self.pw, self.apikey)
-        headers = [("Authorization", f"Bearer {token}")]
+        try:
+            token = self.get_token(self.un, self.pw, self.apikey)
+            headers = [("Authorization", f"Bearer {token}")]
+        except Exception as err:
+            print("Unable to get token. Check your credentials: ", err)
+            return
         async for ws in websockets.connect(
             "wss://gateway.realtime.platts.com/websocket/v1/subscribe",
             extra_headers=headers,
             ping_interval=None,
         ):
             try:
-                logging.info("test")
+                print("Connected. Check log file for details.")
                 for m in self.markets:
                     await ws.send(self.create_subscription_msg(m))
                 async for msg in ws:
@@ -62,6 +84,11 @@ class WSClient:
             except websockets.ConnectionClosed as err:
                 logging.info("Disconnected! Reconnecting in 3 seconds", err)
                 await asyncio.sleep(3)
-                token = get_token.get_token(self.un, self.pw, self.apikey)
-                headers = [("Authorization", f"Bearer {token}")]
+
+                try:
+                    token = self.get_token(self.un, self.pw, self.apikey)
+                    headers = [("Authorization", f"Bearer {token}")]
+                except Exception as err:
+                    print("Unable to get token. Check your credentials: ", err)
+                    return
                 continue
